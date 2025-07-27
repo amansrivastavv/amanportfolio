92c589dfc4199a3f24af2c2363e5bf7d8e0f003f
Aman <amansrivastavvv@gmai.com>
2025-07-27 10:56:18 +0530

    WIP: temporary save before revert

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,6 @@ import BlogsPage from "./pages/BlogsPage";
 import AppLayout from "./App";
 import "./index.css";
 import "locomotive-scroll/dist/locomotive-scroll.css";
-import AboutDetails from "./pages/AboutDetails";
 
 const router = createBrowserRouter(
   [
@@ -28,7 +27,6 @@ const router = createBrowserRouter(
         { path: "blogs", element: <BlogsPage /> },
         { path: "featured-projects", element: <FeaturedProjects /> },
         { path: "all-projects", element: <ProjectsMain /> },
-        { path: "about-details", element: <AboutDetails /> },
       ],
     },
   ],
